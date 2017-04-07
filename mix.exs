d7ad1d4d278e940240277cef2e0a44429343ca7a
agnaldo4j <agnaldo4j@gmail.com>
2017-04-07 18:22:07 -0300

    ci integration

--- mix.exs
+++ mix.exs
@@ -2,11 +2,31 @@ defmodule Prevalence.Mixfile do
   use Mix.Project
 
   def project do
-    [apps_path: "apps",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+        apps_path: "apps",
+        build_embedded: Mix.env == :prod,
+        start_permanent: Mix.env == :prod,
+        deps: deps(),
+        version: "0.0.1",
+        elixir: "1.2.0",
+        description: description(),
+        package: package()
+    ]
   end
+
+    defp description do
+        """"""
+    end
+
+    defp package do
+    [
+        # These are the default files included in the package
+        name: :postgrex,
+        files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+        maintainers: ["Agnaldo de Oliveira"],
+        licenses: ["Apache 2.0"],
+        links: %{"GitHub" => "https://github.com/agnaldo4j/prevalence"}]
+    end
 
   # Dependencies can be Hex packages:
   #
