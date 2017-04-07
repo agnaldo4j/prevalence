4f954ddf3b8ee57a659763f2c02fdf185492e87e
agnaldo4j <agnaldo4j@gmail.com>
2017-04-07 18:10:51 -0300

    ci integration

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule Prevalence.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    [{:credo, "~> 0.7", only: [:dev, :test]}]
+    [
+        {:credo, "~> 0.7", only: [:dev, :test]},
+        {:inch_ex, only: :docs}
+    ]
   end
 end
