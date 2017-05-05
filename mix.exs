93d153416f8492366ded7f89b30fed99d9e44b23
agnaldo4j <agnaldo4j@gmail.com>
2017-05-05 12:05:21 -0300

    fix coverage

--- mix.exs
+++ mix.exs
@@ -2,12 +2,15 @@ defmodule Prevalence.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :prevalence,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :prevalence,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      test_coverage: [tool: Coverex.Task]
+    ]
   end
 
   # Configuration for the OTP application
