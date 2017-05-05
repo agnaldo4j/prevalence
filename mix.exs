58dbe9d55b51cf3c22ff525bcaf0cfd8980766b5
agnaldo4j <agnaldo4j@gmail.com>
2017-05-05 13:24:09 -0300

    fix hex

--- mix.exs
+++ mix.exs
@@ -3,13 +3,34 @@ defmodule Prevalence.Mixfile do
 
   def project do
     [
-      app: :prevalence,
+      app: :prevayler-iex,
       version: "0.1.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
-      test_coverage: [tool: Coverex.Task]
+      test_coverage: [tool: Coverex.Task],
+      description: description(),
+      package: package(),
+      name: "prevayler-iex",
+      source_url: "https://github.com/agnaldo4j/prevalence.git"
+    ]
+  end
+
+  defp description do
+    """
+    Simple implementation of prevalence in Elixir, this project is based on prevayler project (https://github.com/jsampson/prevayler.git).
+    """
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :prevayler-iex,
+      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Agnaldo de Oliveira"],
+      licenses: ["BSD 3-clause"],
+      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
     ]
   end
 
