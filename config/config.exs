977ae009d0ceb8c8aeb540dfc29490333276411d
agnaldo4j <agnaldo4j@gmail.com>
2017-05-09 17:06:32 -0300

    fix configuration

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,10 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :prevayler_iex, Prevalent.Journaling,
+       snapshot_path: "db/snapshot",
+       commands_path: "db/commands"
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
--- lib/prevalent/prevalent_journaling.ex
+++ lib/prevalent/prevalent_journaling.ex
@@ -8,9 +8,9 @@ defmodule Prevalent.Journaling do
     end
 
     def log_command(command) do
-        File.mkdir_p("commands")
+        File.mkdir_p(commands_path())
         time = Integer.to_string(:os.system_time(:milli_seconds))
-        File.cd!("commands", fn() -> write_binary("command_" <> time <> ".dat", command) end)
+        File.cd!(commands_path(), fn() -> write_binary(time <> "_command.dat", command) end)
         command
     end
 
@@ -21,8 +21,8 @@ defmodule Prevalent.Journaling do
     end
 
     def load_snapshot do
-        File.mkdir_p("snapshot")
-        File.cd!("snapshot", fn() ->
+        File.mkdir_p(snapshot_path())
+        File.cd!(snapshot_path(), fn() ->
             case File.read("prevalent_system.dat") do
                 {:ok, system_binary} -> :erlang.binary_to_term(system_binary)
                 {:error, _error} -> %{}
@@ -31,31 +31,43 @@ defmodule Prevalent.Journaling do
     end
 
     def take_snapshot(actual_state) do
-      File.mkdir_p("snapshot")
-      File.cd!("snapshot", fn() -> write_binary("prevalent_system.dat", actual_state) end)
+      File.mkdir_p(snapshot_path())
+      File.cd!(snapshot_path(), fn() -> write_binary("prevalent_system.dat", actual_state) end)
       delete_all_commands()
       actual_state
     end
 
     def delete_all_commands do
-      File.cd!("commands", fn() ->
+      File.cd!(commands_path(), fn() ->
           {:ok, list_of_commands} = File.ls(".")
           Enum.each(list_of_commands, fn(path) -> File.rm(path) end)
       end)
     end
 
     def load_list_of_commands do
-        File.mkdir_p("commands")
-        File.cd!("commands", fn() ->
+        File.mkdir_p(commands_path())
+        File.cd!(commands_path(), fn() ->
             {:ok, list_of_commands} = File.ls(".")
             list_of_commands
         end)
     end
 
     def load_command(path) do
-      case File.cd!("commands", fn() -> File.read(path) end) do
+      case File.cd!(commands_path(), fn() -> File.read(path) end) do
         {:ok, binary} -> :erlang.binary_to_term(binary)
         {:error, _reason} -> {fn(actual_state, _data) -> actual_state end, ""}
       end
+    end
+
+    defp snapshot_path() do
+        configs = Application.get_env(:prevayler_iex, Prevalent.Journaling)
+        IO.puts("snapshot: ")
+        configs[:snapshot_path]
+    end
+
+    defp commands_path() do
+        configs = Application.get_env(:prevayler_iex, Prevalent.Journaling)
+        IO.puts("commands: ")
+        configs[:commands_path]
     end
 end
